e9e588d10c163838f8668c075a733bc02209e3bd
svoby42 <qwerta009@gmail.com>
2022-01-08 21:23:26 +0100

    categories

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Category } from '../entities/category.model';
+import { AuthenticationService } from './authentication.service';
+import { RequestBaseService } from './request-base.service';
+
+const API_URL = `${environment.BASE_URL}/api/categories`
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService extends RequestBaseService{
+
+  constructor(authenticationService: AuthenticationService, http: HttpClient) {
+    super(authenticationService, http);
+  }
+
+  getAllCategories(): Observable<any>{
+    return this.http.get(API_URL);
+  }
+
+  saveCategory(category: Category): Observable<any>{
+    return this.http.post(API_URL, category, {headers: this.getHeaders});
+  }
+
+  deleteCategory(category: Category): Observable<any>{
+    return this.http.delete(`${API_URL}/${category.id}`, {headers: this.getHeaders});
+  }
+
+  
+
+}
