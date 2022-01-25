1edc18af1b95a2cc5ee9621418e9de58b34d74d9
svoby42 <qwerta009@gmail.com>
2022-01-25 18:11:01 +0100

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://www.svoby.eu'
+  BASE_URL: 'http://localhost:8080'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'https://www.svoby.eu'
+  BASE_URL: 'http://localhost:8080'
 };
 
 /*
