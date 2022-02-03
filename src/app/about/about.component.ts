1a34db599b2eea62fd3ea785554dda3631d613cc
svoby42 <qwerta009@gmail.com>
2022-02-03 20:17:40 +0100

    about page v3

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  isVisible: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeDisplay(){
+    this.isVisible = !this.isVisible;
+    return this.isVisible;
   }
 
 }
